a5a95f8e74475999c4e4c4e719280e2c62c91375
r567tw <r567tw@gmail.com>
2018-08-29 22:29:50 +0800

    [feature] add countDown Name

--- src/browser_action/all.js
+++ src/browser_action/all.js
@@ -1,17 +1,20 @@
 var countdownDom = document.getElementById('countdown')
 var limit = 0;
 
-chrome.storage.sync.get('limit',function(countdown){
-    limit = countdown.limit;
+chrome.storage.sync.get('limitDate',function(countdown){
+    limit = countdown.limitDate;
     var now = new Date().getTime();
     var special = Date.parse(limit);
     countdownNumber=Math.ceil((now-special)/(24*60*60*1000));
-    if (countdownNumber < 0)
-    {
-        countdownDom.innerHTML = '距離'+limit+'倒數'+Math.abs(countdownNumber)+'天';
-    }
-    else
-    {
-        countdownDom.innerHTML = '距離'+limit+'已過了'+countdownNumber+'天';
-    }
+    chrome.storage.sync.get('limitName',function(countdown){
+        var name = countdown.limitName
+        if (countdownNumber < 0)
+        {
+            countdownDom.innerHTML = '距離'+name+'倒數'+Math.abs(countdownNumber)+'天';
+        }
+        else
+        {
+            countdownDom.innerHTML = '距離'+name+'已過了'+countdownNumber+'天';
+        }
+    })
 });
--- src/option/option.js
+++ src/option/option.js
@@ -1,13 +1,20 @@
-limitDom = document.getElementById('limit')
+limitDateDom = document.getElementById('limitDate')
+limitNameDom = document.getElementById('limitName')
 submitDom = document.getElementById('send')
 
-chrome.storage.sync.get('limit',function(countdown){
-    limitDom.value = countdown.limit;
+
+chrome.storage.sync.get('limitName',function(countdown){
+    limitNameDom.value = countdown.limitName;
+});
+
+chrome.storage.sync.get('limitDate',function(countdown){
+    limitDateDom.value = countdown.limitDate;
 });
 
 submitDom.addEventListener("click",function(){
-    var limitVal = limitDom.value;
-    chrome.storage.sync.set({'limit':limitVal},function(){
+    var limitDateval = limitDateDom.value
+    var limitNameval = limitNameDom.value;
+    chrome.storage.sync.set({'limitName':limitNameval,'limitDate':limitDateval},function(){
         alert('儲存成功')
     });
 });
