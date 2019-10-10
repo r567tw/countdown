fb7c400520cce6e3ec4313633fde99e42fb92c70
r567tw <r567tw@gmail.com>
2019-10-11 02:08:58 +0800

    beautify popup page

--- src/browser_action/all.js
+++ src/browser_action/all.js
@@ -1,28 +1,32 @@
-var countdownDom = document.getElementById('countdown')
+var countdownDom = document.getElementById("countdown");
 
-chrome.storage.sync.get('countDownConfig',function(result){
-    config = result.countDownConfig;
-    len = result.countDownConfig.length;
-    now = new Date().getTime();
-    countdownDom.innerHTML = '';
-    for (let index = 0; index < len; index++) {
-        Datename= config[index].DateName;
-        if (Datename=='')
-        {
-            Datename =config[index].DateVal;
-        }
-        DateVal = config[index].DateVal;
-        special = Date.parse(DateVal);
-        countdownNumber=Math.ceil((now-special)/(24*60*60*1000));
-        if (countdownNumber < 0)
-        {
-            countdownDom.innerHTML += '距離'+Datename+'倒數'+Math.abs(countdownNumber)+'天<br/>';
-        }
-        else
-        {
-            countdownDom.innerHTML += '距離'+Datename+'已過了'+countdownNumber+'天<br/>';
-        }     
+chrome.storage.sync.get("countDownConfig", function(result) {
+  config = result.countDownConfig;
+  len = result.countDownConfig.length;
+  now = new Date().getTime();
+  countdownDom.innerHTML = "";
+  for (let index = 0; index < len; index++) {
+    Datename = config[index].DateName;
+    if (Datename == "") {
+      Datename = config[index].DateVal;
     }
+    DateVal = config[index].DateVal;
+    special = Date.parse(DateVal);
+    countdownNumber = Math.ceil((now - special) / (24 * 60 * 60 * 1000));
+    if (countdownNumber < 0) {
+      countdownDom.innerHTML +=
+        "<div class='info'>距離" +
+        Datename +
+        "倒數" +
+        Math.abs(countdownNumber) +
+        "天</div>";
+    } else {
+      countdownDom.innerHTML +=
+        "<div class='info'>距離" +
+        Datename +
+        "已過了" +
+        countdownNumber +
+        "天</div>";
+    }
+  }
 });
-
-
